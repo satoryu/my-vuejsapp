82a5d2418e23a6e3880e97f72b1f2555caf35f5f
Tatsuya Sato <tatsuya.sato.j4v@jp.denso.com>
2019-09-25 15:06:23 +0900

    CleanupWebpack's usage has changed

--- webpack.dev.js
+++ webpack.dev.js
@@ -25,7 +25,7 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	module: {
